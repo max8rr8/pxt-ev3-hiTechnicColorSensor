a249c95b554fd9f513bccf1e86adae2ff2062124
Max_ <mrom06@ya.ru>
2019-07-13 13:19:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# hitechnicColorSensor
+
+Library for using hitechnic color sensor require IIC
+
+## TODO
+
+- [ ] Add a reference for your blocks here
+- [ ] Add "icon.png" image (300x200) in the root folder
+- [ ] Add "- beta" to the GitHub project description if you are still iterating it.
+- [ ] Turn on your automated build on https://travis-ci.org
+- [ ] Use "pxt bump" to create a tagged release on GitHub
+- [ ] Get your package reviewed and approved https://makecode.mindstorms.com/packages/approval
+
+Read more at https://makecode.mindstorms.com/packages/build-your-own
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/linux
+(The metadata above is needed for package search.)
+
---
+++ main.ts
@@ -0,0 +1,34 @@
+namespace sensors {
+
+  class HiTechnicColorSensor extends sensors.internal.IICSensor {
+    getColor() {
+        this.transaction(1, [66], 1)
+        return this.getBytes()[0]
+    }
+  
+    getRGB() {
+        this.transaction(1, [67], 3)
+        return this.getBytes()
+    }
+  
+    getWhite() {
+        this.transaction(1, [70], 1)
+        return this.getBytes()[0]
+    }
+  
+    getAll() {
+        this.transaction(1, [66], 5)
+        return this.getBytes()
+    }
+  
+    _IICId(){
+        control.dmesg(readIICID(2, 0))
+        return 'HiTechncColorPD'
+    }
+  }
+
+  export const hitechnicColor1 = new HiTechnicColorSensor(1)
+  export const hitechnicColor2 = new HiTechnicColorSensor(2)
+  export const hitechnicColor3 = new HiTechnicColorSensor(3)
+  export const hitechnicColor4 = new HiTechnicColorSensor(4)
+}
